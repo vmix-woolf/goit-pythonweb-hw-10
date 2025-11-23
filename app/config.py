e847a0a06526072634bcb7889f77e2bdf811a1cf
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-11-23 11:07:32 +0200

    Step 15: implement JWT authentication

--- app/config.py
+++ app/config.py
@@ -1,14 +1,34 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: str
-
+    # Database
     postgres_user: str
     postgres_password: str
     postgres_db: str
+    database_url: str
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    # JWT auth
+    secret_key: str
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 60
+
+    # SMTP
+    smtp_host: str
+    smtp_port: int
+    smtp_user: str
+    smtp_password: str
+    mail_from: str
+
+    # Cloudinary
+    cloudinary_name: str
+    cloudinary_api_key: str
+    cloudinary_api_secret: str
+
+    model_config = {
+        "env_file": ".env",
+        "extra": "forbid"
+    }
 
 
 settings = Settings()
