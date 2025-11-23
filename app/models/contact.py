46bb5c5486ee40e074dbf8f25b932f221f77008e
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-11-23 10:40:53 +0200

    Step 14: feat: implement User CRUD with password hashing

--- app/models/contact.py
+++ app/models/contact.py
@@ -1,14 +1,20 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
 from app.models.base import Base
 
 
 class Contact(Base):
     __tablename__ = "contacts"
 
-    id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, index=True)
-    last_name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-    phone = Column(String)
-    birthday = Column(Date)
-    additional_info = Column(String, nullable=True)
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    email = Column(String(255), unique=False, nullable=False)
+    phone = Column(String(50), nullable=False)
+    birthday = Column(Date, nullable=True)
+    additional_info = Column(String(255), nullable=True)
+
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    # Зв’язок з користувачем
+    owner = relationship("User", back_populates="contacts")
