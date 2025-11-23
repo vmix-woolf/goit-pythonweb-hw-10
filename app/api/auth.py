7e9a09153b403231f4a90f0434980b6be396f8d0
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-11-23 19:55:41 +0200

    Step 18: add rate limiting for /me endpoint

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.schemas.user import UserCreate, UserOut, UserLogin
 from app.services.email import send_verification_email
-from app.services.auth import create_access_token, verify_token
+from app.services.auth import create_access_token, verify_token, get_current_user
 from app.crud.user import create_user, get_user_by_email
 from app.database import get_session
+from app.services.limiter import limiter
+from app.models.user import User
+from fastapi import Request
+
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
@@ -65,3 +68,10 @@ async def verify_email(token: str, session: AsyncSession = Depends(get_session))
 
     return JSONResponse({"message": "Email successfully verified"})
 
+@router.get("/me")
+@limiter.limit("5/minute")
+async def get_me(
+    request: Request,
+    current_user: User = Depends(get_current_user),
+):
+    return current_user
--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi.middleware import SlowAPIMiddleware
+from app.services.limiter import limiter
 from app.api.auth import router as auth_router
 from app.api.contacts import router as contacts_router
 
 app = FastAPI(title="Contacts API")
 
-app.include_router(auth_router)  # prefix вєе є в auth.py
-app.include_router(contacts_router)  # prefix вже є в contacts.py
+app.state.limiter = limiter
+app.add_middleware(SlowAPIMiddleware)
 
-# CORS
+app.include_router(auth_router)
+app.include_router(contacts_router, prefix="/contacts")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,6 +20,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.get("/")
 def root():
